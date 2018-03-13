6472b891b06ddc594b29e8d3392233687a00bd7a
tomaszkononowicz <tomaszkononowicz@gmail.com>
2018-03-13 16:09:34 +0100

    GUI, images

---
+++ Cryptography/ValidationRules/PasswordValidation.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Cryptography.ValidationRules
+{
+    class PasswordValidation : ValidationRule
+    {
+        public int? MinLength { get; set; }
+        public int? MaxLength { get; set; }
+        public string Pattern { get; set; }
+        public string PatternErrorMessage { get; set; }
+        public bool CanBeEmpty { get; set; } = false;
+        public bool IsEnabled { get; set; } = true;
+
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            if (IsEnabled)
+            {
+                int i = 0;
+                if (value != null)
+                    i = value.ToString().Length;
+                if (!(CanBeEmpty) && i <= 0)
+                    return new ValidationResult(false, "To pole nie może być puste");
+                else if (i < (MinLength ?? i))
+                    return new ValidationResult(false, "Wprowadzony tekst jest zbyt krótki, minimalna liczba znaków: " + MinLength);
+                else if (i > (MaxLength ?? i))
+                    return new ValidationResult(false, "Wprowadzony tekst jest zbyt długi, maksymalna liczba znaków: " + MaxLength);
+                else if (Pattern != null && !Regex.IsMatch(value.ToString(), Pattern))
+                    return new ValidationResult(false, "Wprowadzony tekst nie pasuje do wzorca: " + PatternErrorMessage);
+                else
+                    return ValidationResult.ValidResult;
+            }
+            else return ValidationResult.ValidResult;
+        }
+
+    }
+}
--- Cryptography/Views/MainWindow.xaml.cs
+++ Cryptography/Views/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Cryptography
+namespace Cryptography.Views
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -24,5 +24,6 @@ namespace Cryptography
         {
             InitializeComponent();
         }
+
     }
 }
